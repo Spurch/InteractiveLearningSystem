72c6e2b995a6fded388a876438fb39858d670734
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-02-07 02:56:01 +0200

    Added some UI for the Admin panel

--- InteractiveLearningSystem.Web/Areas/Admin/Controllers/AdminController.cs
+++ InteractiveLearningSystem.Web/Areas/Admin/Controllers/AdminController.cs
@@ -1,13 +1,15 @@
 ﻿namespace InteractiveLearningSystem.Web.Areas.Admin.Controllers
 {
     using System.Web.Mvc;
-   
+    using InteractiveLearningSystem.Data;
+    using System.Linq;
     public class AdminController : AdminBaseController
     {
         // GET: Admin/Admin
         public ActionResult Index()
         {
-            return View();
+            var Users = InteractiveLearningSystemDbContext.Create().Users.ToList();
+            return View(Users);
         }
     }
 }
