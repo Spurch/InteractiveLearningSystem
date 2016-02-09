c47763e40ad7117ac35301db7f299a27d77cab9a
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-02-09 17:00:04 +0200

    Added jQuery ajax to the project
    Also a little work on the services.

---
+++ InteractiveLearningSystem.Services/UserServices.cs
@@ -0,0 +1,21 @@
+﻿namespace InteractiveLearningSystem.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Constracts;
+    using Data.Repositories;
+    using Models;
+
+    public class UserServices : IUserServices
+    {
+        private IRepository<User> users;
+
+        public UserServices(IRepository<User> users)
+        {
+            this.users = users;
+        }
+    }
+}
