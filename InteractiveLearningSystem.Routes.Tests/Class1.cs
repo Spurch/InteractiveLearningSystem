b2e31a63609a0dde1d3c125d94ae2a8ce5d686a0
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-02-16 15:56:13 +0200

    Added initial test projects to ILS Solution

--- InteractiveLearningSystem.Routes.Tests/Class1.cs
+++ InteractiveLearningSystem.Routes.Tests/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InteractiveLearningSystem.Tests
+namespace InteractiveLearningSystem.Routes.Tests
 {
     public class Class1
     {
--- InteractiveLearningSystem.Services.Tests/Properties/AssemblyInfo.cs
+++ InteractiveLearningSystem.Services.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InteractiveLearningSystem.Tests")]
+[assembly: AssemblyTitle("InteractiveLearningSystem.Services.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InteractiveLearningSystem.Tests")]
+[assembly: AssemblyProduct("InteractiveLearningSystem.Services.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("67ac3052-7379-4f0e-8a6a-8620f5fc7232")]
+[assembly: Guid("032df1d4-fe59-4deb-bb1a-a6e8625e6cc2")]
 
 // Version information for an assembly consists of the following four values:
 //
