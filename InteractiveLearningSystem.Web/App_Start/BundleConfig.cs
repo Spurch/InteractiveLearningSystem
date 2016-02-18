b0b70f94798509aaddd0b68f6210317caa5e49f8
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-02-18 17:15:16 +0200

    Configured KendoMVC for the project

--- InteractiveLearningSystem.Web/App_Start/BundleConfig.cs
+++ InteractiveLearningSystem.Web/App_Start/BundleConfig.cs
@@ -24,9 +24,16 @@ namespace InteractiveLearningSystem.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                     "~/Scripts/KendoUI/kendo.all.min.js",
+                      "~/Scripts/KendoUI/kendo.aspnetmvc.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                     "~/Content/KendoUI/kendo.common.min.css",
+                     "~/Content/KendoUI/kendo.default.min.css"));
         }
     }
 }
