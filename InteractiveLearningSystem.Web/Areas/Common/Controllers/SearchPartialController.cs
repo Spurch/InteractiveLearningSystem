a329526c17132c667679b03ae2c8b3938467ecaf
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-02-21 01:08:36 +0200

    Kendo autocomplete working for the Admin view
    Need to make a special SchoolViewModel for the Autocomplete.

--- InteractiveLearningSystem.Web/Areas/Common/Controllers/SearchPartialController.cs
+++ InteractiveLearningSystem.Web/Areas/Common/Controllers/SearchPartialController.cs
@@ -1,13 +1,39 @@
 ﻿namespace InteractiveLearningSystem.Web.Areas.Common.Controllers
 {
+    using Microsoft.AspNet.Identity;
+    using Services;
+    using System.Linq;
     using System.Web.Mvc;
 
-    public class SearchPartialController : Controller
+    public class SearchPartialController : BaseController
     {
+        public SearchPartialController(RoleServices roleServices,
+            UserServices userServices, SchoolServices schoolServices)
+        {
+            this.userServices = userServices;
+            this.roleServices = roleServices;
+            this.schoolServices = schoolServices;
+        }
+
         public ActionResult SearchPartial()
         {
+            var id = User.Identity.GetUserId();
+            var user = userServices.GetById(id);
+            var roles = userServices.GetUserRoles(id);
 
-            return PartialView("_SearchPartial");
+            return PartialView("_SearchPartial", roles);
+        }
+
+        [HttpGet]
+        public ActionResult SchoolAutocomplete(string text)
+        {
+            var result = schoolServices.GetAll()
+                .Where(x => x.Name.ToLower()
+                .Contains(text.ToLower()))
+                .Select(x => x.Name)
+                .ToArray();
+
+            return this.Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
