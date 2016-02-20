78717b263ef22861fecd5f889285e9bd89df5310
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-02-20 20:06:35 +0200

    Moved the SearchBar to a partial
    Created separate SearchPartialController in the Common area. Logic to be
    implemented

---
+++ InteractiveLearningSystem.Web/Areas/Common/Controllers/SearchPartialController.cs
@@ -0,0 +1,13 @@
+﻿namespace InteractiveLearningSystem.Web.Areas.Common.Controllers
+{
+    using System.Web.Mvc;
+
+    public class SearchPartialController : Controller
+    {
+        public ActionResult SearchPartial()
+        {
+
+            return PartialView("_SearchPartial");
+        }
+    }
+}
