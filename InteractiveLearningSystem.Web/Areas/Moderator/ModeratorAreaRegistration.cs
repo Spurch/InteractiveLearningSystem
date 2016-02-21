70e825a761012f908f57e58e39c62767b3cff764
Ivan Nikolov <ivan.stoyanov.nikolov@gmail.com>
2016-02-21 14:03:07 +0200

    Fixed some AreaRegistration issues

--- InteractiveLearningSystem.Web/Areas/Moderator/ModeratorAreaRegistration.cs
+++ InteractiveLearningSystem.Web/Areas/Moderator/ModeratorAreaRegistration.cs
@@ -17,7 +17,7 @@
             context.MapRoute(
                 "Moderator_default",
                 "Moderator/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
